39451b1988a55d69c093753bb229a4915794c334
vikas jadaun <deadpoolji125@gmail.com>
2023-07-29 00:04:47 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,19 @@ export default function Navbar() {
   return (
     <div>
       <div id="navbar">
-          <ul>
-               <li><a href="/index.html">home</a></li>
-               
-          </ul>
+          <div className="navbar">
+               <div id="logo">Crypt0-Coin</div>
+               <div id="ul">
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">home</a></li>
+               </div>
+               <div id="nav3">
+                    <li><a href="#">tu</a></li>
+                    <li><a href="#">ty</a></li>
+               </div>
+          </div>
       </div>
     </div>
   )
