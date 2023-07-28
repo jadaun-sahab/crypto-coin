9abf83cdb73ef7a95c6a07496b1b6e8f8a9551c3
vikas jadaun <deadpoolji125@gmail.com>
2023-07-28 23:39:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    
+    <Navbar/>
     </>
     
   )
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../style/navbar.css";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div id="navbar">
+          <ul>
+               <li><a href="/index.html">home</a></li>
+               
+          </ul>
+      </div>
+    </div>
+  )
+}
