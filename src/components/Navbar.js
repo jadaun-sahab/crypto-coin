f4e27f9a725f8cbdbf98b04ccae81d9aed3ce91c
vikas jadaun <deadpoolji125@gmail.com>
2023-08-04 18:07:53 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,14 @@ export default function Navbar() {
           <div className="navbar">
                <div id="logo">CRYPTIC</div>
                <div id="ul">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Market</a></li>
-                    <li><a href="#">Why Choose us</a></li>
-                    <li><a href="#">Join us</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Market</a></li>
+                    <li><a href="/">Why Choose us</a></li>
+                    <li><a href="/">Join us</a></li>
                </div>
                <div id="nav3">
-                    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa-solid fa-gamepad"></i></a></li>
+                    <li><a href="/"><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="/"><i class="fa-solid fa-gamepad"></i></a></li>
                </div>
           </div>
       </div>
