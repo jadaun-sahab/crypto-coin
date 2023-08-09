f53df31a782049d9668022b0c7a44e00738c1636
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-09 23:26:33 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Router>
+    <Routes>
+      <Route exact path="/About" element={<About/>}>
+      </Route>
+      <Route exact path="/" element={
+        <TextForm showAlert={showAlert} heading="Enter The Text Here To Analyze" mode={mode}/>}>
+      </Route>
+    </Routes>
     <Navbar/>
     <Home/>
+    </Router>
     </>
     
   )
