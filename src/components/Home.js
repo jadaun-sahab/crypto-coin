ac4277b1fe2710a8619cd77df98981049ce5bf88
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-10 21:00:09 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,10 @@ import React from 'react'
 import "../style/home.css";
 import img from '../img/img1.png';
 import img2 from '../img/img2.png';
+import img3 from '../img/bitcoin.webp';
+import img4 from '../img/ethereum.webp';
+import img5 from '../img/Tether.webp';
+import img6 from '../img/bnb-icon2_2x.webp';
 
 export default function Home() {
   return (
@@ -16,10 +20,26 @@ export default function Home() {
           <h1>CRYPTO CURRENCIES</h1>
         </div>
         <div className="heading3">
-        <img src={img2} alt="Logo" />;
-        <img src={img2} alt="Logo" />;
-        <img src={img2} alt="Logo" />;
-        <img src={img2} alt="Logo" />;
+          <div className="child">
+            <img src={img3} alt="Logo"/>;
+            <h2>Bitcoin<span>-1.6%</span></h2>
+            <p>$29,34,66.00</p>
+          </div>
+          <div className="child">
+            <img src={img4} alt="Logo"/>;
+            <h2>Bitcoin<span>-1.6%</span></h2>
+            <p>$29,34,66.00</p>
+          </div>
+          <div className="child">
+            <img src={img5} alt="Logo"/>;
+            <h2>Bitcoin<span>-1.6%</span></h2>
+            <p>$29,34,66.00</p>
+          </div>
+          <div className="child">
+            <img src={img6} alt="Logo"/>;
+            <h2>Bitcoin<span>-1.6%</span></h2>
+            <p>$29,34,66.00</p>
+          </div>
         </div>
       </div>
     </>
