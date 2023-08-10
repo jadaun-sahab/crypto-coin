e8d545ab8ec5b046225f8f640a5ec0638f40a012
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-10 19:25:57 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import "../style/home.css";
 // import '../../public/logo192.png'
+import img from '../img/img1.png';
+import img2 from '../img/img2.png';
 
 export default function Home() {
   return (
     <>
       <div className="home">
         <div className="heading">
-          <img src='../../public/logo192.png' height={'200px'} width={'200px'} alt="" />
+          <img src={img} alt="Logo" />;
           <h1>TRACK AND TRADE</h1>
-          <img src="" alt="" />
+          <img src={img2} alt="Logo" />;
         </div>
         
       </div>
