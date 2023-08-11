dd399f8f915378dbd483d3d925aa32f65c1c1b87
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-11 16:13:32 +0530

    update

--- src/components/Whychoose.js
+++ src/components/Whychoose.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react";
 import "../style/why.css";
+import img from "../img/img7.png";
 
 export default function Whychoose() {
   return (
-    <div className='why'>
-      lorem777
+    <div className="why">
+      <div id="maincontainer">
+        <div className="childcontainer">
+          <div className="child"></div>
+          <div className="child"></div>
+          <div className="child"></div>
+        </div>
+        <div className="childcontainer">
+          <img src={img} alt="" />;
+        </div>
+        <div className="childcontainer">
+          <div className="child"></div>
+          <div className="child"></div>
+          <div className="child"></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
