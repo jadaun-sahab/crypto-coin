eb3e2fdcf6e33e5bc8fe2aa5752cd4378bf48807
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-11 21:21:20 +0530

    update

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import '../style/join.css';
+import img from '../img/img1.png';
+import img2 from '../img/img2.png';
 
 export default function Join() {
   return (
-    <div>
-      <ul>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
-      </ul>
+    <div className='join'>
+        <div className="heading">
+          <img src={img} alt="Logo" />;
+          <h1>JOIN US VIA</h1>
+          <img src={img2} alt="Logo" />;
+        </div>
+
+        <div className="heading2">
+          <h1>DISCORD</h1>
+        </div>
+
+        <div className="joinheding3">
+          <p>Invest and manage all your crypto at one place.</p>
+        </div>
+        
+        <div>
+          <button className="joinbtn">Join via Discord</button>
+        </div>
     </div>
   );
 }
