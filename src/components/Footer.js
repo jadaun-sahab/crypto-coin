7a89e125d8994f7effd3d3596ed7ea6977324f61
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-12 18:19:09 +0530

    udate

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import img from '../img/img1.png'
+import React from 'react';
+import img from '../img/img9.png';
+import '../style/footer.css';
+
 export default function Footer() {
   return (
     <div id='footer'>
       <div className="footer">
         <img src={img} alt="" />
         <div className="footerchild">
-          
+<div className="footerchild1">
+  
+</div>
         </div>
-        <img src={img} alt="" />
+       
       </div>
       
     </div>
