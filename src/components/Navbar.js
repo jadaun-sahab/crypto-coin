1ad1e723e378eb13fd427cf27b131e8695090cc9
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-13 01:16:04 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,33 @@
 import React from 'react'
 import "../style/navbar.css";
 import { Link } from 'react-router-dom';
+import React1, { useState } from 'react';
 
-// const toggleButton = document.getElementsByClassName('fa-bars')[0]
-//   const navbarLinks = document.getElementsByClassName('navbar')[0]
- 
-//   toggleButton.addEventListener('click', () => {
-//       navbarLinks.classList.toggle('active')
-//   })
 export default function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleToggleClick = () => {
+    setIsActive(!isActive);
+  };
   return (
     
     <>
       <div id="navbar">
      
-          <div className="navbar">
+          <div className='navbar'>
                <div id="logo">CRYPTIC</div>
-               <div id="ul">
+               <div className={`navbar2 ${isActive ? 'active' : ''}`}>
                     <li><Link to="/Home">Home</Link></li>
                     <li><Link to="/Market">Market</Link></li>
                     <li><Link to="/Whychoose">Why Choose us</Link></li>
                     <li><Link to="/Join">Join us</Link></li>
                </div>
                <div id="nav3">
-                    <li><a href="/"><i class="fa-brands fa-twitter"></i></a></li>
-                    <li><a href="/"><i class="fa-solid fa-gamepad"></i></a></li>
+                    <li><a href="/"><i className="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="/"><i className="fa-solid fa-gamepad"></i></a></li>
                </div>
           </div>
-          <i class="fa-solid fa-bars" onclick="menuitem()"></i>
+          <i className="fa fa-bars" onClick={handleToggleClick}></i>
       </div>
     </>
   )
